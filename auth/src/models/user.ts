ffe7ac9492c4dd43a70574fbaa0db3ac4d88104a
Kibria10 <junakkibria56@gmail.com>
2021-11-29 12:12:17 +0600

    hashing integration

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,44 +1,47 @@
-import mongoose, { mongo }  from 'mongoose';
-//An interface that describes the properties
-//that are required to create a new user
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+// An interface that describes the properties
+// that are requried to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
 
-interface UserAttrs{
-    email: String;
-    password: String;
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
 }
-//An interface that  describes the properties
-//that a User Model has
-interface UserModel extends mongoose.Model<UserDoc>{
-    build(attrs: UserAttrs): UserDoc;
-}
-//An interface that describes the properties
-//that a user document has or a single user has
-interface UserDoc extends mongoose.Document{
-    email: String;
-    password: String;
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
-
+userSchema.pre("save", async function (done) {
+  //isModified middlware is to prevent from hashing a pw which is already been hashed
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
 userSchema.statics.build = (attrs: UserAttrs) => {
-    return new User(attrs);
+  return new User(attrs);
 };
 
-const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
-const user = User.build({
-    email: 'test@test.com',
-    password: 'test123'
-});
-
-
-export {User};
+export { User };
