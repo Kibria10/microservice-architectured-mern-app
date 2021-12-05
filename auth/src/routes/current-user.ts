e2eb85b28b7ead00a97162a6c1c0673171dc6bdb
Kibria10 <junakkibria56@gmail.com>
2021-12-05 10:49:18 +0600

    finalized api

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
 import { requireAuth } from '../middlewares/require-auth';
 const router = express.Router();
