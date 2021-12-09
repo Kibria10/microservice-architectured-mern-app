09c4244062cac15adce59f27ee0a4f2504691767
Kibria10 <junakkibria56@gmail.com>
2021-12-09 15:28:53 +0600

    integrating react app

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -29,8 +29,6 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      //   console.log('Email in use');
-      //   return res.send({});
       throw new BadRequestError('Email in use');
     }
 
