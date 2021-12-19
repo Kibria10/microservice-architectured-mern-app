fdcac0a3e58b172e23b1956128b82e50ab80bf77
Kibria10 <junakkibria56@gmail.com>
2021-12-19 11:33:15 +0600

    onSuccess cb for routing

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown'
+import Dropdown from 'react-bootstrap/Dropdown';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
@@ -9,93 +10,82 @@ export default () => {
   const [name, setName] = useState('');
   const [role, setRole] = useState('');
   const [department, setDepartment] = useState('');
-
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+      name,
+      role,
+      department
+    },
+    onSuccess: () => Router.push('/')
+  });
 
   const onSubmit = async event => {
     event.preventDefault();
-
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-        name,
-        role,
-        department
-      });
-
-      console.log(response.data);
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    await doRequest();
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <h1><center><i>Shurjo ERP Registration Form</i></center></h1>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          type="password"
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <label>Name</label>
-        <input
-          value={name}
-          onChange={e => setName(e.target.value)}
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <br></br>
-      <div className="form-group">
-      <label>Role</label>
-      <DropdownButton title="Select Role"
-               onSelect={e=>setRole(e)}>
-              <Dropdown.Item eventKey="Senior HR">Senior HR</Dropdown.Item>
-              <Dropdown.Item eventKey="Senior Accountant">Senior Accountant</Dropdown.Item>
-              <Dropdown.Item eventKey="Jr. Software Developer">Jr. Software Developer</Dropdown.Item>
-              <Dropdown.Item eventKey="Sr. Software Developer">Sr. Software Developer</Dropdown.Item>
-              <Dropdown.Item eventKey="Product Manager">Product Manager</Dropdown.Item>
-              <Dropdown.Item eventKey="Intern">Intern</Dropdown.Item>
+    <h1><center><i>Shurjo ERP Registration Form</i></center></h1>
+    <div className="form-group">
+      <label>Email Address</label>
+      <input
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        className="form-control"
+      />
+    </div>
+    <div className="form-group">
+      <label>Password</label>
+      <input
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        type="password"
+        className="form-control"
+      />
+    </div>
+    <div className="form-group">
+      <label>Name</label>
+      <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+        type="text"
+        className="form-control"
+      />
+    </div>
+    <br></br>
+    <div className="form-group">
+    <label>Role</label>
+    <DropdownButton title="Select Role"
+             onSelect={e=>setRole(e)}>
+            <Dropdown.Item eventKey="Senior HR">Senior HR</Dropdown.Item>
+            <Dropdown.Item eventKey="Senior Accountant">Senior Accountant</Dropdown.Item>
+            <Dropdown.Item eventKey="Jr. Software Developer">Jr. Software Developer</Dropdown.Item>
+            <Dropdown.Item eventKey="Sr. Software Developer">Sr. Software Developer</Dropdown.Item>
+            <Dropdown.Item eventKey="Product Manager">Product Manager</Dropdown.Item>
+            <Dropdown.Item eventKey="Intern">Intern</Dropdown.Item>
+    </DropdownButton>
+    </div>
+    <br></br>
+    <div className="form-group">
+      <label>Department</label>
+      <DropdownButton title="Select Department"
+             onSelect={e=>setDepartment(e)}>
+            <Dropdown.Item eventKey="HR">HR</Dropdown.Item>
+            <Dropdown.Item eventKey="Accounting">Accounting</Dropdown.Item>
+            <Dropdown.Item eventKey="Technology">Technology</Dropdown.Item>
+            <Dropdown.Item eventKey="Sales">Sales</Dropdown.Item>
       </DropdownButton>
-      </div>
-      <br></br>
-      <div className="form-group">
-        <label>Department</label>
-        <DropdownButton title="Select Department"
-               onSelect={e=>setDepartment(e)}>
-              <Dropdown.Item eventKey="HR">HR</Dropdown.Item>
-              <Dropdown.Item eventKey="Accounting">Accounting</Dropdown.Item>
-              <Dropdown.Item eventKey="Technology">Technology</Dropdown.Item>
-              <Dropdown.Item eventKey="Sales">Sales</Dropdown.Item>
-        </DropdownButton>
 
-      </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Sorry,</h4>
-          <ul className="my-0">
-            {errors.map(err => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+    </div>
       <br></br>
-      <center><button className="btn btn-primary">Sign Up</button> </center>
+      {errors}
+      <br></br>
+      <center><button className="btn btn-primary">Sign Up</button></center>
     </form>
   );
 };
