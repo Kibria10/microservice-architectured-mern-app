054e18d3fafa2df08820862985c0817ed093ba87
Kibria10 <junakkibria56@gmail.com>
2021-12-21 12:08:07 +0600

    get initial props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  console.log(data);
+
+  return data;
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,52 +1,19 @@
-// import buildClient from "../api/build-client";
-// const LandingPage = ({ currentUser }) => {
-//   console.log(currentUser);
+import buildClient from '../api/build-client';
 
-//   return <h1>Landing Page</h1>;
-// };
-
-// LandingPage.getInitialProps = async context => {
-//   const client = buildClient(context);
-//   const { data } = await client.get('/api/users/currentuser');
-//   return data;
-// };
-// export default LandingPage;
-
-
-import axios from 'axios';
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return currentUser ? <h1>You are signed in...  <p><a href="http://ticketing.dev/auth/signout">Sign Out</a></p>
-  </h1> : <h1>You are not signed in...   <p><a href="http://ticketing.dev/auth/signin">Sign In</a></p>
-</h1>;
-
-
+  return currentUser ? (
+    <h1>You are signed in.. <p><a href="http://ticketing.dev/auth/signout">Sign Out</a></p></h1>
+  ) : (
+    <h1>You are NOT signed in.. <p><a href="http://ticketing.dev/auth/signin">Sign In</a></p></h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
-    console.log("SSR-URL");
-    const { data } = await axios.get(
-        
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers
-      }
-      
-    );
+LandingPage.getInitialProps = async context => {
+  console.log('LANDING PAGE!');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    console.log("Browser-URL");
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
-  return {};
+  return data;
 };
 
 export default LandingPage;
