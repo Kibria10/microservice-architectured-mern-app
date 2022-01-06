230f4e7a480ca2114e76315cee49249f1bbe8dd5
Kibria10 <junakkibria56@gmail.com>
2022-01-06 11:38:57 +0600

    update routes testing environment

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,7 +6,7 @@ import { errorHandler, NotFoundError, currentUser } from '@kibria10/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes/index';
-
+import { updateTicketRouter } from './routes/update';
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -21,7 +21,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
-
+app.use(updateTicketRouter);
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
