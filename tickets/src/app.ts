355cbcef83d7d49595815df53e0d0bc806c93892
Kibria10 <junakkibria56@gmail.com>
2022-01-06 11:54:35 +0600

    update routes debugging

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError, currentUser } from '@kibria10/common';
+import { errorHandler, NotFoundError, currentUser } from '@sgtickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes/index';
 import { updateTicketRouter } from './routes/update';
+
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -22,6 +23,7 @@ app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
 app.use(updateTicketRouter);
+
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
